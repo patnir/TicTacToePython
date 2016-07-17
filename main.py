acaa80b5839aded4b4d57eff14c9bc7cf0959b81
patnir <rahul20patni@gmail.com>
2016-07-17 13:06:06 -0400

    Logic to draw Xs and Os for specific cell-sizes

---
+++ main.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 17 12:47:23 2016
+
+@author: Rahul Patni
+"""
+import sys
+
+# Tic Tac Toe
+
+CELL_SIZE = 3
+
+def drawBoard():
+    return 
+    
+def drawX(x, y):
+    for i in range(CELL_SIZE):
+        for j in range(CELL_SIZE):
+            if (i == 0 or i == CELL_SIZE - 1) and (j == 0 or j == CELL_SIZE - 1):
+                sys.stdout.write("+")
+            elif (i == j):
+                sys.stdout.write("+")
+            elif i == CELL_SIZE - j - 1:
+                sys.stdout.write("+")
+            else:
+                sys.stdout.write(" ")
+        sys.stdout.write("\n")
+    return
+
+def drawO(x, y):
+    for i in range(CELL_SIZE):
+        for j in range(CELL_SIZE):
+            if i == 0 or i == CELL_SIZE - 1 or j == 0 or j == CELL_SIZE - 1:
+                sys.stdout.write("o")
+            else: 
+                sys.stdout.write(" ")
+        sys.stdout.write("\n")
+    return
+    
+def main():
+    drawBoard()
+    drawO(0,0)
+    
+if __name__ == "__main__":
+    main()
