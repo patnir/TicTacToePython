f08fa030d207d1ac7c391ebb303375b7ad59de8e
patnir <rahul20patni@gmail.com>
2016-07-17 14:40:34 -0400

    Drawing Xs and Os in boxes

--- main.py
+++ main.py
@@ -11,31 +11,7 @@ import sys
 CELL_SIZE = 5
 
 gBoard = []
-    
-def drawX(x, y):
-    for i in range(CELL_SIZE):
-        for j in range(CELL_SIZE):
-            if (i == 0 or i == CELL_SIZE - 1) and (j == 0 or j == CELL_SIZE - 1):
-                sys.stdout.write("+")
-            elif (i == j):
-                sys.stdout.write("+")
-            elif i == CELL_SIZE - j - 1:
-                sys.stdout.write("+")
-            else:
-                sys.stdout.write(" ")
-        sys.stdout.write("\n")
-    return
 
-def drawO(x, y):
-    for i in range(CELL_SIZE):
-        for j in range(CELL_SIZE):
-            if i == 0 or i == CELL_SIZE - 1 or j == 0 or j == CELL_SIZE - 1:
-                sys.stdout.write("o")
-            else: 
-                sys.stdout.write(" ")
-        sys.stdout.write("\n")
-    return
-    
 def initializeBoard(dim):
     [gBoard.append([' '] * dim) for num in range(dim)]
     length = len(gBoard)
@@ -48,19 +24,41 @@ def initializeBoard(dim):
             for j in range(length):
                 if j % (CELL_SIZE + 1) == 0:
                     gBoard[i][j] = "|"
-    return gBoard    
-
-
+    return gBoard 
+    
 def printBoard():
     for i in range(len(gBoard)):
         for j in range(len(gBoard)):
             sys.stdout.write(gBoard[i][j]),
         sys.stdout.write("\n")
+
+def drawX(x, y):
+    xOffset = (x - 1) * (CELL_SIZE + 1) + 1
+    yOffset = (y - 1) * (CELL_SIZE + 1) + 1
+    for i in range(CELL_SIZE):
+        for j in range(CELL_SIZE):
+            if (i == 0 or i == CELL_SIZE - 1) and (j == 0 or j == CELL_SIZE - 1):
+                gBoard[i + yOffset][j + xOffset] = "+"
+            elif (i == j):
+                gBoard[i + yOffset][j + xOffset] = "+"
+            elif i == CELL_SIZE - j - 1:
+                gBoard[i + yOffset][j + xOffset] = "+"
+    return
+
+def drawO(x, y):
+    xOffset = (x - 1) * (CELL_SIZE + 1) + 1
+    yOffset = (y - 1) * (CELL_SIZE + 1) + 1
+    for i in range(CELL_SIZE):
+        for j in range(CELL_SIZE):
+            if i == 0 or i == CELL_SIZE - 1 or j == 0 or j == CELL_SIZE - 1:
+                gBoard[i + yOffset][j + xOffset] = "o"
+    return   
     
 def main():
     initializeBoard(3 * (CELL_SIZE + 1) + 1)
+    drawX(3, 3)
+    drawO(3, 2)
     printBoard()
-    drawO(0,0)
     
 if __name__ == "__main__":
     main()
