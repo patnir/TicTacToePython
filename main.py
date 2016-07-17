9a3c02f406ee8917b7e04c8b28723b21201a2b71
patnir <rahul20patni@gmail.com>
2016-07-17 14:24:34 -0400

    Initialize board

--- main.py
+++ main.py
@@ -8,25 +8,9 @@ import sys
 
 # Tic Tac Toe
 
-CELL_SIZE = 3
+CELL_SIZE = 5
 
-def drawBoard():
-    length = 3 * (CELL_SIZE + 1) + 1
-    for i in range(length):
-        if i % (CELL_SIZE + 1) == 0:
-            for j in range(length):
-                if j % (CELL_SIZE + 1) == 0:
-                    sys.stdout.write(" ")
-                else:
-                    sys.stdout.write("-")
-        else:
-            for j in range(length):
-                if j % (CELL_SIZE + 1) == 0:
-                    sys.stdout.write("|")
-                else:
-                    sys.stdout.write(" ")
-        sys.stdout.write("\n")
-    return 
+gBoard = []
     
 def drawX(x, y):
     for i in range(CELL_SIZE):
@@ -52,9 +36,31 @@ def drawO(x, y):
         sys.stdout.write("\n")
     return
     
+def initializeBoard(dim):
+    [gBoard.append([' '] * dim) for num in range(dim)]
+    length = len(gBoard)
+    for i in range(length):
+        if i % (CELL_SIZE + 1) == 0:
+            for j in range(length):
+                if j % (CELL_SIZE + 1) != 0:
+                    gBoard[i][j] = "-"
+        else:
+            for j in range(length):
+                if j % (CELL_SIZE + 1) == 0:
+                    gBoard[i][j] = "|"
+    return gBoard    
+
+
+def printBoard():
+    for i in range(len(gBoard)):
+        for j in range(len(gBoard)):
+            sys.stdout.write(gBoard[i][j]),
+        sys.stdout.write("\n")
+    
 def main():
-    drawBoard()
-    #drawO(0,0)
+    initializeBoard(3 * (CELL_SIZE + 1) + 1)
+    printBoard()
+    drawO(0,0)
     
 if __name__ == "__main__":
     main()
